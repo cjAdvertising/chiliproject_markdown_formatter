f1d053bb818114188de3241ee5ee7199df7d08f9
Greg Thornton <xdissent@me.com>
2012-10-01 07:58:03 -0500

    Make initial page content match toolbar header style and fix stylesheet includes.

--- app/helpers/chiliproject_markdown_formatter/helper.rb
+++ app/helpers/chiliproject_markdown_formatter/helper.rb
@@ -3,6 +3,7 @@ module ChiliprojectMarkdownFormatter
     unloadable
 
     def wikitoolbar_for(field_id)
+      heads_for_wiki_formatter
       
       url = Engines::RailsExtensions::AssetHelpers.plugin_asset_path('chiliproject_markdown_formatter', 'help', 'markdown_syntax.html')
 
@@ -16,13 +17,17 @@ module ChiliprojectMarkdownFormatter
         javascript_tag("var toolbar = new jsToolBar($('#{field_id}')); toolbar.setHelpLink('#{help_link}'); toolbar.draw();")
     end
 
-
     def initial_page_content(page)
-      "#{page.pretty_title}\n#{'='*page.pretty_title.length}"
+      "# #{page.pretty_title.to_s} #"
     end
 
     def heads_for_wiki_formatter
-      stylesheet_link_tag('jstoolbar')
+      unless @heads_for_wiki_formatter_included
+        content_for :header_tags do
+          stylesheet_link_tag('jstoolbar') + stylesheet_link_tag('markdown', :plugin => 'chiliproject_markdown_formatter')
+        end
+        @heads_for_wiki_formatter_included = true
+      end
     end
   end
 end
